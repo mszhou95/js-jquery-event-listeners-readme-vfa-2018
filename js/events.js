f2435c2414551e98b23d188b09910c94606938dd
Michele Zhou <michele.zhou@gmail.com>
2018-06-29 21:35:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,49 @@
 //define functions here
 
+
+//getIt function
+function getIt() {
+  $('p').click(function() {
+    alert("Hey!");
+  }
+)}
+
+//frameIt function
+
+function frameIt() {
+  
+  $('img').addClass('tasty')
+    
+}
+
+//pressIt function
+
+function pressIt() {
+  
+  $('#typing').keydown(function(key) {
+    
+    if(key.which == 71) {
+      alert("g was pressed");
+    }
+  })
+}
+
+//submitIt function
+function submitIt() {
+  
+  $("form").on("submit", function() {
+    
+    alert("Your form is going to be submitted now.")
+    
+  })
+  
+}
+
 $(document).ready(function(){
+  
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+})
 
-// call functions here
-
-});
