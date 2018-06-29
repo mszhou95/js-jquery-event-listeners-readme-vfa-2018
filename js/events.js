cdfb0fbf0cfd111a859e6224f8798574865d804c
Michele Zhou <michele.zhou@gmail.com>
2018-06-29 20:59:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,10 @@
 //define functions here
 
-  $('p').on("click", function getIt(){
+function getIt() {
+  $('p').on("click", function(){
     alert "Hey!";
   }
-)
+)}
 
 $(document).ready(function(){
   
